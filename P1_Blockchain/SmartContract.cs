c69533831ca5b0711a6386dc7c29b125bf3b1896
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2023-12-13 11:38:23 +0100

    Add props to SmartContract

--- P1_Blockchain/SmartContract.cs
+++ P1_Blockchain/SmartContract.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace P1_Blockchain
 {
     public class SmartContract
     {
-        
+        public List<Miner> Miners { get; set; }
+        public List<User> Users { get; set; }
     }
 }
