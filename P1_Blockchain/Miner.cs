f87cc6e2b107756712eda032beafd31a08172bc4
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2023-12-13 11:46:55 +0100

    Update register method

--- P1_Blockchain/Miner.cs
+++ P1_Blockchain/Miner.cs
@@ -1,12 +1,19 @@
+using System;
+
 namespace P1_Blockchain 
 {
     public class Miner
     {
         public int Id { get; set; }
         public double BTC { get; set; }
-        void Register(SmartContract smartContract)
+        public void Register(SmartContract smartContract)
         {
-            //TODO
+            if(!smartContract.Miners.Contains(this))
+            {
+                smartContract.Miners.Add(this);
+            } else {
+                throw new Exception("Miner already registered to smart contract");
+            }
         }
     }
 }
