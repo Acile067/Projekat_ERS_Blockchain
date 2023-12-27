fcfc4dc37c5acffbf50c8531c928c01ed037f5d8
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2023-12-27 14:12:23 +0100

    Klasa za prenos poruka

---
+++ CommonInterfaces/Classes/DataMessage.cs
@@ -0,0 +1,9 @@
+namespace CommonInterfaces
+{
+    public class DataMessage
+    {
+        public int UserId { get; set; }
+        public string Data { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
