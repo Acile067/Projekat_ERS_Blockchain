53ad9e8c9322b1e2bcf87070a83122f86e2b07f5
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2023-12-27 10:17:56 +0100

    Uprostiti mejn klijenta

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,16 +5,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string? data;
-        if(args.Length >= 2)
-        {
-            data = args[1];
-        } 
-        else 
-        {
-            Console.Write("Enter the data you want to send: ");
-            data = Console.ReadLine();
-        }
+        Console.Write("Enter the data you want to send: ");
+        string? data = Console.ReadLine();
+        
         var client = new Client(0, data!); //TODO: proveriti da li je data null
         var conService = new ClientConnectionService();
         var uiHandler = new ClientUIHandler(client, conService);
