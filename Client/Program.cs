877af5d5d225f481758e03147da20fcd6a7dbd87
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2023-12-27 14:09:47 +0100

    Update klijentov mejn

--- Client/Program.cs
+++ Client/Program.cs
@@ -5,13 +5,12 @@ internal class Program
 {
     private async static Task Main(string[] args)
     {
-        //Console.Write("Enter the data you want to send: ");
-        //string? data = Console.ReadLine();
-        
         var client = new Client(); //TODO: proveriti da li je data null
         await client.Register();
+        Console.WriteLine("Client registered successfuly!");
         Console.WriteLine(client);
-        //var uiHandler = new ClientUIHandler(client, conService);
-        //uiHandler.HandleUI();
+        
+        var uiHandler = new ClientUIHandler(client);
+        uiHandler.HandleUI();
     }
 }
