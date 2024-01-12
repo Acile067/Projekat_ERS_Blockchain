b0f8789b07b7c6be36330e4e10dd66b2316b9a8e
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-01-12 03:50:41 +0100

    Simplify code

--- CommonInterfaces/Services/ClientRegisterService.cs
+++ CommonInterfaces/Services/ClientRegisterService.cs
@@ -9,27 +9,23 @@ namespace CommonInterfaces
     {
         public async Task<IUser?> Register()
         {
-            using (var tcpClient = new TcpClient(AddressFamily.InterNetwork))
+            using var tcpClient = new TcpClient(AddressFamily.InterNetwork);
+            try
             {
-                try
-                {
-                    await tcpClient.ConnectAsync(IPAddress.Loopback, 8080);
-                    using (var stream = tcpClient.GetStream())
-                    {
-                        var buffer = new byte[512];
+                await tcpClient.ConnectAsync(IPAddress.Loopback, 8080);
+                using var stream = tcpClient.GetStream();
+                var buffer = new byte[512];
 
-                        await stream.WriteAsync(Encoding.UTF8.GetBytes("CLIENT"));
-                        int length = await stream.ReadAsync(buffer);
-                        string clientJson = Encoding.UTF8.GetString(buffer, 0, length);
-                        var client = JsonSerializer.Deserialize<Client>(clientJson);
-                        return client;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Connection error: {0}", ex.Message);
-                    throw;
-                }
+                await stream.WriteAsync(Encoding.UTF8.GetBytes("CLIENT"));
+                int length = await stream.ReadAsync(buffer);
+                string clientJson = Encoding.UTF8.GetString(buffer, 0, length);
+                var client = JsonSerializer.Deserialize<Client>(clientJson);
+                return client;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection error: {0}", ex.Message);
+                throw;
             }
         }
     }
--- CommonInterfaces/Services/MinerRegisterService.cs
+++ CommonInterfaces/Services/MinerRegisterService.cs
@@ -13,27 +13,23 @@ namespace CommonInterfaces.Services
     {
         public async Task<IUser?> Register()
         {
-            using (var tcpMiner = new TcpClient(AddressFamily.InterNetwork))
+            using var tcpMiner = new TcpClient(AddressFamily.InterNetwork);
+            try
             {
-                try
-                {
-                    await tcpMiner.ConnectAsync(IPAddress.Loopback, 8080);
-                    using (var stream = tcpMiner.GetStream())
-                    {
-                        var buffer = new byte[512];
+                await tcpMiner.ConnectAsync(IPAddress.Loopback, 8080);
+                using var stream = tcpMiner.GetStream();
+                var buffer = new byte[512];
 
-                        await stream.WriteAsync(Encoding.UTF8.GetBytes("MINER"));
-                        int length = await stream.ReadAsync(buffer);
-                        string minerJson = Encoding.UTF8.GetString(buffer, 0, length);
-                        var miner = JsonSerializer.Deserialize<Miner>(minerJson);
-                        return miner;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Connection error: {0}", ex.Message);
-                    throw;
-                }
+                await stream.WriteAsync(Encoding.UTF8.GetBytes("MINER"));
+                int length = await stream.ReadAsync(buffer);
+                string minerJson = Encoding.UTF8.GetString(buffer, 0, length);
+                var miner = JsonSerializer.Deserialize<Miner>(minerJson);
+                return miner;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Connection error: {0}", ex.Message);
+                throw;
             }
         }
     }
